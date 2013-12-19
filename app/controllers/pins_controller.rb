ecf780d88bbfc803b530ce46d91fdd7bf365bf52
Nancy <li.nancy73@gmail.com>
2013-12-19 15:09:29 -0700

    Add Endless Scroll

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -5,12 +5,13 @@ class PinsController < ApplicationController
   # GET /pins
   # GET /pins.json
 
-  def index
+   def index
     @pins = Pin.order("created_at desc").page(params[:page]).per_page(20)
 
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @pins }
+      format.js
     end
   end
 
